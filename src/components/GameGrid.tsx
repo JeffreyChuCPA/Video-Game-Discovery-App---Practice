52dbc90cc715c008c59ed6e6067e28ff48a7cba8
Jeffrey Chu <jeffcolinchu@gmail.com>
2023-11-03 17:00:28 -0400

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,25 @@
 import useGames from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-    const { games, error } = useGames(); //*pulling games and error props from the useGames function via destructuring
+    const { games, error, isLoading } = useGames(); //*pulling games and error props from the useGames function via destructuring
+
+    const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
+            <SimpleGrid
+                columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+                padding="10px"
+                spacing={10}
+            >
+                {isLoading &&
+                    skeletons.map((skeleton) => (
+                        <GameCardSkeleton key={skeleton} />
+                    ))}
                 {games.map((game) => (
                     <GameCard key={game.id} game={game} />
                 ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,22 +28,29 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]); //*Stores the games from api. Empty array initialized, but of type Game[] defined above
     const [error, setError] = useState(''); //*set error message
+    const [isLoading, setLoading] = useState(false)
 
     //*to send a fetch request to backend. useEffect, always add the [] as dependency in second parameter
     useEffect(() => {
         const controller = new AbortController()
 
+        setLoading(true);
         apiClient
             .get<FetchGamesResponse>('/games', {signal: controller.signal})
-            .then(res => setGames(res.data.results)) //* res var is type FetchGamesResponse, so we can then get .results property. res var has property and responses from Axios, use data prop so we can read body of response
+            .then(res => {
+                setGames(res.data.results); //* res var is type FetchGamesResponse, so we can then get .results property. res var has property and responses from Axios, use data prop so we can read body of response
+                setLoading(false);
+            }) 
             .catch(err => {
                 if (err instanceof CanceledError) return;
-                setError(err.message)});
+                setError(err.message);
+                setLoading(false);
+            });
 
         return () => controller.abort();
     }, []);
     
-    return { games, error }; //*return games and error objects to be used in components
+    return { games, error, isLoading }; //*return games and error objects to be used in components
 }
 
 export default useGames;
